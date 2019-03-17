67a172abbef9a60d01c5ffde7590c885f3e657cd
Douglas Franklin <douglasfkln@outlook.com>
2019-03-17 11:20:09 -0300

    create interface

--- App.js
+++ App.js
@@ -8,6 +8,13 @@ export default class App extends React.Component {
 
   constructor(props) {
     super(props);
+    /* status
+    0: aguardando convidado
+    1: minha vez
+    2: vez do oponente */
+    /* jogada
+    1: criador
+    2: convidado */
     this.state = {
       c00: '-',
       c01: '-',
@@ -27,10 +34,95 @@ export default class App extends React.Component {
   }
 
   render() {
+    const { c00, c01, c02, c10, c11, c12, c20, c21, c22, iniciado, showDialog, convidado, codJogo } = this.state;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        {iniciado ? (
+          <View style={styles.container}>
+            {convidado ? (
+              <View style={styles.container}>
+                <View style={{
+                  justifyContent: 'space-around',
+                  flexDirection: 'row'
+                }}>
+                  <TouchableOpacity onPress={this.onPress00}>
+                    <Text style={styles.texto}>{c00}</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={this.onPress01}>
+                    <Text style={styles.texto}>
+                      {c01}
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={this.onPress02}>
+                    <Text style={styles.texto}>
+                      {c02}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                <View style={{ justifyContent: 'space-around', flexDirection: 'row' }}>
+                  <TouchableOpacity onPress={this.onPress10}>
+                    <Text style={styles.texto}>
+                      {c10}
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={this.onPress11}>
+                    <Text style={styles.texto}>
+                      {c11}
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={this.onPress12}>
+                    <Text style={styles.texto}>
+                      {c12}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                <View style={{ justifyContent: 'space-around', flexDirection: 'row' }}>
+                  <TouchableOpacity onPress={this.onPress20}>
+                    <Text style={styles.texto}>
+                      {c20}
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={this.onPress21}>
+                    <Text style={styles.texto}>
+                      {c21}
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={this.onPress22}>
+                    <Text style={styles.texto}>
+                      {c22}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ) : (
+                <View style={styles.containerQrCode}>
+                  <QRCode value={codJogo} size={200} />
+                </View>
+              )
+            }
+          </View>) :
+          (
+            <View style={styles.container}>
+              <TouchableOpacity onPress={this.criar}>
+                <Text style={styles.texto}>Criar Jogo</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={this.entrar}>
+                <Text style={styles.texto}>Entrar em um Jogo</Text>
+              </TouchableOpacity>
+            </View>
+          )
+        }
+        <Dialog.Container visible={showDialog}>
+          <Dialog.Title>Entrar no Jogo</Dialog.Title>
+          <Dialog.Description>
+            Digite abaixo o código do Jogo
+          </Dialog.Description>
+          <Dialog.Input
+            onChangeText={inputCodJogo => this.setState({ inputCodJogo })}
+          />
+          <Dialog.Button label="ENTRAR" onPress={this.entrarNoJogo} />
+        </Dialog.Container>
+      </View >
     );
   }
 }
